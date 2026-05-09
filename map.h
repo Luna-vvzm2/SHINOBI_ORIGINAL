26313700366f2d770c67dd68a0abcd56a7efe7a7
Luna-vvzm2 <luna72344929@yahoo.co.jp>
2026-05-09 13:41:46 +0900

    可変ジャンプ

--- map.h
+++ map.h
@@ -1,6 +1,11 @@
 #pragma once
+const int mapWidth = 190;
+const int mapHeight = 20;
 
-void MapLoad();
+static int mapData[mapHeight][mapWidth];
+static int objData[mapHeight][mapWidth];
+
+void MapLoad(const char* mapfilename, const char* objfilename);
 void MapUpdate();
 void MapDraw();
 
